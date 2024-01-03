971f74819578ea091c65957085656c64c8632c41
Manmohanbabu Rupanagudi <89210693+pandu-0@users.noreply.github.com>
2024-01-02 20:43:39 -0800

    Update Main.svelte
    Commented out Video Button in Main.svelte

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -113,14 +113,18 @@ let steps = [
                 Curious to <span class="poppins text-violet-400">see</span> my work?
             </h3>
         </div>
-        <a
+
+        <!-- Video Button and Link -->
+        <!-- <a
             href="https://www.youtube.com/watch?v=wh9QLjk3M2k"
             target="_blank"
             class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
         >
             <i class="fa-regular fa-circle-play" />
             <p>Watch the video</p>
-        </a>
+        </a> -->
+
+        <!-- PROJECT DETAILS -->
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
 
             <!-- PROJECT 1 -->
