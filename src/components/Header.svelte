9d8e6ad7ace0ec4473ca5e06525d4e0f347fa5da
Manmohanbabu Rupanagudi <89210693+pandu-0@users.noreply.github.com>
2024-01-13 12:34:10 -0800

    resume fix

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -61,7 +61,7 @@
         <!-- Resume -->
         <a style="border: 2px solid MediumPurple;
             justify-content: center; align-items: center;display: flex;"
-            href="../static/resume.pdf"
+            href="static/resume.pdf"
             target="_blank"
             class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full 
             text-slate-950">
